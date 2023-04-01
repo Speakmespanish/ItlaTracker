99a92e307058455787bfd082070e4064b0543c71
Speakmespanish <andysonvargasmedina170@hotmail.com>
2023-04-01 03:58:40 -0400

    Quedo incompleto, no me pasaron las vistas, son las 4:am y tengo sueno, aparte de que estoy enfermo :(

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CharlieComponent} from './Mapa/charlie/charlie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/reservas',
+  component: CharlieComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor() {}
+  isMenuOpen = false;
   title = 'testingangular';
+
+  toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu?.classList.toggle('active');
+    }
+
+    
 }
